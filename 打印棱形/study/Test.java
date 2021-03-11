e640d0ddefb763c7b0303e32b19669f777895001
mozutangsan <62471229+mozutangsan@users.noreply.github.com>
2021-03-11 22:19:56 +0800

    Create Test.java

---
+++ 打印棱形/study/Test.java
@@ -0,0 +1,34 @@
+package study;
+import java.util.Scanner;
+public class Test1 {
+public static void main(String[] args){
+Scanner sca=new Scanner(System.in);
+int a=sca.nextInt();
+for(int b=a;b>0;b--){
+  for(int c=b;c>1;c--){
+    System.out.print(" ");
+  }
+  System.out.print("*");
+  if(b<a){
+    for(int c=(a-b)*2;c>1;c--){
+      System.out.print(" ");
+    }
+    System.out.print("*");
+  }
+  System.out.println();
+}
+for(int b=2;b<a+1;b++){
+  for(int c=b;c>1;c--){
+    System.out.print(" ");
+  }
+  System.out.print("*");
+  if(b<a){
+    for(int c=(a-b)*2;c>1;c--){
+      System.out.print(" ");
+    }
+    System.out.print("*");
+  }
+  System.out.println();
+}
+}
+}
