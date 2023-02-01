0e996b2f62d19bf2083558a139983471696bece2
mozutangsan <2245141324@qq.com>
2023-02-01 08:19:52 +0800

    1

--- src/study/mySQL.java
+++ src/study/mySQL.java
@@ -9,19 +9,19 @@ public interface mySQL {
     /**
      * 初始化数据库链接并导入数据
      */
-    public void start();
+    void start();
 
     /**
      * 关闭数据库并清楚数据
      */
-    public void stop();
+    void stop();
     /**
      * 为数据库表格插入指定名称和内容的数据
      * @param column_name
      * @param column_type
      * @throws SQLException
      */
-    public void insertValue(String column_name,int column_type) throws SQLException;
+    void insertValue(String column_name,int column_type) throws SQLException;
     /**
      * 传入指定数据的名称和内容，返回同一个数据的另一个指定数据名称的内容。
      * @param column_name1
@@ -30,7 +30,7 @@ public interface mySQL {
      * @return 查询数据结果
      * @throws SQLException
      */
-    public String getType(String column_name1,String column_type,String column_name2) throws SQLException;
+    String getType(String column_name1,String column_type,String column_name2) throws SQLException;
     /**
      * 传入指定数据的名称和int型内容，将指定名称的内容改为指定内容
      * @param column_name1
@@ -39,5 +39,5 @@ public interface mySQL {
      * @param column_type2
      * @throws SQLException
      */
-    public void updateValue(String column_name1,int column_type1,String column_name2,String column_type2) throws SQLException;
+    void updateValue(String column_name1,int column_type1,String column_name2,String column_type2) throws SQLException;
 }
